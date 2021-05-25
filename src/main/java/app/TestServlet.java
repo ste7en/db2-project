0c4c576c6721fa11fa68bb0fbf9b63c5d5828cb3
Stefano Formicola <stefanoformicola@icloud.com>
2021-05-25 21:51:59 +0200

    web.xml moved from WebContent/WEB-INF to webapp/WEB-INF

--- src/main/java/app/TestServlet.java
+++ src/main/java/app/TestServlet.java
@@ -2,14 +2,13 @@ package app;
 
 import java.io.*;
 import javax.servlet.*;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
-@WebServlet("/TestServlet")
+
 public class TestServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	protected void doGet(HttpServletRequest request, HttpServletResponse
-			response)
-					throws ServletException, IOException {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		response.setContentType("text/plain");
 		PrintWriter out = response.getWriter();
 		out.println("Ciao fai fotografia");
