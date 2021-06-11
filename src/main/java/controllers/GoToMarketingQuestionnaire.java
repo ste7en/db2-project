f2e02e713ec884858c342054fcc4246b960fe355
Andrea <andrea.alpy@gmail.com>
2021-06-11 17:48:59 +0200

    added GoToMarketingQuestionnaire

--- src/main/java/controllers/GoToMarketingQuestionnaire.java
+++ src/main/java/controllers/GoToMarketingQuestionnaire.java
@@ -1,6 +1,8 @@
 package controllers;
 
 import java.io.IOException;
+import java.util.Date;
+import java.util.List;
 
 import javax.ejb.EJB;
 import javax.servlet.ServletContext;
@@ -9,11 +11,16 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 import org.thymeleaf.templatemode.TemplateMode;
 import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+import model.MarketingQuestion;
+import services.MarketingQuestionnaireService;
+import services.ProductOfTheDayService;
 
 /**
  * Servlet implementation class GoMarketingQuestionnaire
@@ -21,13 +28,14 @@ import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
 
 @WebServlet("/GotoMarketingQuestionnaire")
-public class GotoMarketingQuestionnaire extends HttpServlet {
+public class GoToMarketingQuestionnaire extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private TemplateEngine templateEngine;
 	@EJB(name = "db2-project.src.main.java.services/MarketingQuestionnaireService")
 	private MarketingQuestionnaireService mqService;
+	private ProductOfTheDayService pService;
 
-	public GotoMarketingQuestionnaire() {
+	public GoToMarketingQuestionnaire() {
 		super();
 	}
 
@@ -50,15 +58,17 @@ public class GotoMarketingQuestionnaire extends HttpServlet {
 			response.sendRedirect(loginpath);
 			return;
 		}
-
+		Date d= new Date();
+		List<MarketingQuestion> marketingQuestions= mqService.findByDate(d);
 		
 
-		// Redirect to the Home page and add missions to the parameters
+		// Redirect to the Home page and add missions to the parameters	
 		String path = "/WEB-INF/Marketing.html";
 		ServletContext servletContext = getServletContext();
 		final WebContext ctx = new WebContext(request, response, servletContext, request.getLocale());
-		
+		ctx.setVariable("marketing questions", marketingQuestions);
 		templateEngine.process(path, ctx, response.getWriter());
+		
 	}
 
 	public void destroy() {
---
+++ src/main/java/services/MarketingQuestionnaireService.java
@@ -0,0 +1,55 @@
+package services;
+
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
+import model.MarketingQuestion;
+import model.ProductOfTheDay;
+
+public class MarketingQuestionnaireService {
+	protected EntityManager em;
+	
+	public MarketingQuestionnaireService(EntityManager em) {
+		this.em=em;
+	}
+	
+	public MarketingQuestion createMarketingQuestion(int number, Date questionnaire_date, String text, ProductOfTheDay p) {
+		MarketingQuestion mq= new MarketingQuestion();
+		mq.setProductOfTheDay(p);
+		mq.setNumber(number);
+		mq.setQuestionnaireDate(questionnaire_date);
+		mq.setText(text);
+		em.persist(mq);
+		return mq;
+	}
+	
+	public void removeMarketingQuestion(int number) {
+		MarketingQuestion mq= findMarketingQuestion(number);
+		if(mq!=null) {
+			em.remove(mq);
+		}
+	}
+	
+	public MarketingQuestion findMarketingQuestion(int number) {
+		return em.find(MarketingQuestion.class,number);
+	}
+	
+	public MarketingQuestion findbyProductOfTheDay(ProductOfTheDay p) {
+		return em.find(MarketingQuestion.class, p);
+	}
+	
+	public List<MarketingQuestion> findByDate(Date d) {
+		return (em.createQuery("SELECT mq from marketing_question mq WHERE mq.date=d", MarketingQuestion.class)).getResultList();
+	}
+	
+	public Collection<MarketingQuestion> findAllMarketingQuestions(){
+		TypedQuery query= em.createQuery("SELECT mq from MARKETINGQUESTION mq", MarketingQuestion.class);
+		return query.getResultList();
+	}
+	
+
+}
