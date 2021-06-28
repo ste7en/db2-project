2ec9903d7cf734bb0f39f0fb3661c33df222c318
Andrea <andrea.alpy@gmail.com>
2021-06-28 12:51:37 +0200

    Added MarketingAnswerService

---
+++ src/main/java/services/MarketingAnswerService.java
@@ -0,0 +1,41 @@
+package services;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+import model.MarketingAnswer;
+import model.MarketingQuestion;
+import model.User;
+
+@Stateless
+public class MarketingAnswerService {
+	@PersistenceContext(unitName = "db2-alparone-ferrara-formicola")
+	protected EntityManager em;
+	
+	public MarketingAnswerService() {
+		
+	}
+	
+	public MarketingAnswer createMarketingAnswer(User user, MarketingQuestion question, String answer) {
+		MarketingAnswer ma= new MarketingAnswer(user,question,answer);
+		em.persist(ma);
+		return ma;
+	}
+	public List<MarketingAnswer> findAllMarketingAnswers(){
+		TypedQuery<MarketingAnswer> query= em.createQuery("SELECT ma from MarketingAnswer ma", MarketingAnswer.class);
+		return query.getResultList();
+	}
+	
+	public List<MarketingAnswer> findByUserAndDate(User u, Date d){
+		TypedQuery<MarketingAnswer> query= em.createNamedQuery("MarketingAnswer.findByUserIdAndDate", MarketingAnswer.class)
+				.setParameter(1, u).setParameter(2, d);
+		return query.getResultList();
+	}
+	
+
+}
