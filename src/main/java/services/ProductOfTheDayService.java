0d6ce9eb34fcb565b1b8817087fd23963741a011
Stefano Formicola <stefanoformicola@icloud.com>
2021-07-06 20:22:10 +0200

    ProductOfTheDayService commented and refactored

--- src/main/java/services/ProductOfTheDayService.java
+++ src/main/java/services/ProductOfTheDayService.java
@@ -15,16 +15,32 @@ public class ProductOfTheDayService {
 	
 	public ProductOfTheDayService() {}
 	
+	/**
+	 * Service method used in the CreationPage for a new questionnaire
+	 * @param p
+	 * @param d
+	 * @return the new created ProductOfTheDay
+	 */
 	public ProductOfTheDay createProductOfTheDay(Product p, Date d) {
 		ProductOfTheDay pofd = new ProductOfTheDay(d, p);
 		em.persist(pofd);
 		return pofd;
 	}
 	
+	/**
+	 * Service method used in the HomePage to retrieve the product of the day
+	 * @param d Date
+	 * @return the product of the day
+	 */
 	public ProductOfTheDay findProductByDate(Date d) {
 		return em.find(ProductOfTheDay.class, d);
 	}
 	
+	/**
+	 * Deletes a ProductOfTheDay corresponding to a date
+	 * @param d Date
+	 * @return true if the product of the day exists, false otherwise
+	 */
 	public boolean removeProductOfTheDay(Date d) {
 		ProductOfTheDay pofd= findProductByDate(d);
 		if(pofd == null) return false;
