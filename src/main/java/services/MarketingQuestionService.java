12a50fd7cf7f678c7383625b31e83e068962fdaf
Stefano Formicola <stefanoformicola@icloud.com>
2021-07-06 20:22:28 +0200

    MarketingQuestionService commented and refactored

--- src/main/java/services/MarketingQuestionService.java
+++ src/main/java/services/MarketingQuestionService.java
@@ -18,28 +18,36 @@ public class MarketingQuestionService {
 	public MarketingQuestionService() {
 	}
 	
+	/**
+	 * Service method used in the @see GoToCreationPage POST request to create a new marketing question
+	 * @param number
+	 * @param questionnaire_date
+	 * @param text
+	 * @param p ProductOfTheDay
+	 * @return a new MarketingQuestion instance
+	 */
 	public MarketingQuestion createMarketingQuestion(int number, Date questionnaire_date, String text, ProductOfTheDay p) {
 		MarketingQuestion mq = new MarketingQuestion(p, number, text);
 		em.persist(mq);
 		return mq;
 	}
 	
-	public void removeMarketingQuestion(Date d, int number) {
-		MarketingQuestion mq = findMarketingQuestion(d, number);
-		if (mq != null) {
-			em.remove(mq);
-		}
-	}
-	
+	/**
+	 * Service method used in the @see SubmitMarketingQuestionnaire POST request to create a relative @see MarketingAnswer
+	 * @param d Date
+	 * @param number
+	 * @return the MarketingQuestion managed entity
+	 */
 	public MarketingQuestion findMarketingQuestion(Date d, int number) {
 		return em.find(MarketingQuestion.class, new MarketingQuestionID(number, d));
 	}
 	
+	/**
+	 * Service method used in the @see GoToMarketingQuestionnaire @see GoToInspectionPage servlets 
+	 * @param d Date
+	 * @return a list of marketing questions corresponding to the date d
+	 */
 	public List<MarketingQuestion> findByDate(Date d) {
 		return em.createNamedQuery("MarketingQuestion.findByDate", MarketingQuestion.class).setParameter(1, d).getResultList();
-	}
-	
-	public List<MarketingQuestion> findAllMarketingQuestions(){
-		return em.createNamedQuery("MarketingQuestion.findAll", MarketingQuestion.class).getResultList();
 	}
 }
